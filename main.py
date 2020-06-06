01e943194799dc34f86fec8e734fb7de2d81318a
Brian Miranda <brian.campos@softtek.com>
2020-06-06 18:51:51 -0500

    Update main.py
    Se agrega funcion para borrar archivos

--- main.py
+++ main.py
@@ -11,9 +11,24 @@ def move(src, dest,extension):
     for file_path in glob.glob(os.path.join(src, '**', '*'+str(extension)), recursive=True):
         new_path = os.path.join(dest, os.path.basename(file_path))
         new_path = new_path.replace("."+str(extension)," - "+str(contador)+"."+str(extension))
-        print ("" + file_path + " - " + new_path)
+        print ("" + file_path + " moved to " + new_path)
         contador = contador + 1
         shutil.move(file_path, new_path)
-    print (str(contador) + " archivos movidos")
+    return contador
 
-move("/Volumes/BRIAN/BCK/", "/Volumes/BRIAN/ALL/JPG/","jpeg")
+def delete(src,extension):
+    contador = 0
+    for file_path in glob.glob(os.path.join(src, '**', '*'+str(extension)), recursive=True):
+        os.chmod(file_path, 777)
+        os.remove(file_path)
+        print ("" + file_path + " deleted")
+        contador = contador + 1
+    return contador
+    
+
+#movidos = move("/Volumes/BRIAN/BCK/", "/Volumes/BRIAN/ALL/JPG/","jpeg")
+
+deleted = delete("/Volumes/BRIAN/BCK/","HEIC")
+
+print (str(deleted) + " files deleted")
+
